16cd172ac05013af52c1aaed153e882c503b1789
James Nylen <jnylen@gmail.com>
2010-08-05 22:25:07 -0400

    removed charguess (no longer needed)

--- openwesabe-installer.sh
+++ openwesabe-installer.sh
@@ -153,7 +153,7 @@ ruby -rrubygems -e "exit 1 if
   sudo ln -sfv /usr/bin/gem1.8 /usr/bin/gem || exit 1
 ) || exit
 
-sudo gem install bundler thor charguess || exit
+sudo gem install bundler thor || exit
 # FIXME: sudo gem install can create ~/.gem as root
 sudo chown -R "`id -un`:`id -gn`" "$dir" ~/.gem || exit 1
 bundle install || exit
